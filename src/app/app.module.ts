dc8284cf150f8bc9f715f56dfb68e2b6fcc1606e
yunusyerli1 <yunus.yerli@gaboras.com>
2022-09-06 22:49:10 +0300

    search bar done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FlagCardComponent } from './components/flag-card/flag-card.component';
+import { HomeComponent } from './home/home.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FlagCardComponent
+    FlagCardComponent,
+    HomeComponent,
+    SearchBarComponent
   ],
   imports: [
     BrowserModule,
